e3bb85fcaaa2fd730746e021fc2e0f85aeea8bb0
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-12 15:58:39 +0300

    redesigned task "TwoFer" #2

--- csharp/two-fer/TwoFer.cs
+++ csharp/two-fer/TwoFer.cs
@@ -1,17 +1,8 @@
-﻿using System;
-
-public static class TwoFer
+﻿public static class TwoFer
 {
-    // In order to get the tests running, first you need to make sure the Speak method 
-    // can be called both without any arguments and also by passing one string argument.
-    public static string Speak(string x=null)
+public static string Speak ( string name=null)
     {
-        string answer;
-          string a=($"One for {x}, one for me.");
-            string b=("One for you, one for me.");
-             answer = x != null ? a:b;
-               return (answer); 
-        }
+     var x= name!=null ?  $"{name}":"you";
+       return $"One for {x}, one for me."; 
     }
-
-
+}
