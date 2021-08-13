7c1ce4c4187eac5fdc74d1b07f58872452320707
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-13 19:14:01 +0300

    Leap (correct titles + corrections)

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -2,10 +2,9 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        bool mainrule = year % 4 == 0;
-        bool divisionby100withremainer = year % 100 != 0;
-        bool divisionby100withoutremainer = year % 100 == 0;
-        bool divisionby400withoutremainer = year % 400 == 0;
-        return ((mainrule && divisionby100withremainer) || (divisionby100withoutremainer && divisionby400withoutremainer));
+        var divisionBy4WithOutRemainder = year % 4 == 0;
+        var divisionBy100WithRemainder = year % 100 != 0;
+        var divisionBy400WithOutRemainder = year % 400 == 0;
+        return (divisionBy4WithOutRemainder && divisionBy100WithRemainder) || divisionBy400WithOutRemainder;
     }
 }
