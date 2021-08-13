75581b9db106ce3d37e72c9a955a10003e454edc
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-13 20:18:42 +0300

    Renamed variables

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -2,9 +2,9 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        var divisionBy4WithOutRemainder = year % 4 == 0;
-        var divisionBy100WithRemainder = year % 100 != 0;
-        var divisionBy400WithOutRemainder = year % 400 == 0;
-        return (divisionBy4WithOutRemainder && divisionBy100WithRemainder) || divisionBy400WithOutRemainder;
+        var isDivisibleBy4 = year % 4 == 0;
+        var isDivisibleBy100 = year % 100 == 0;
+        var isDivisibleBy400 = year % 400 == 0;
+        return (isDivisibleBy4 && !isDivisibleBy100) || isDivisibleBy400;
     }
 }
