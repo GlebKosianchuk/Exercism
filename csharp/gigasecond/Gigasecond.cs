5a3341ef68fa3a087fd898395f188e11beec0941
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-15 09:02:37 +0300

    Gigasecond normal edition

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -1,12 +1,10 @@
 using System;
-
 public static class Gigasecond
 {
     public static DateTime Add(DateTime moment)
     {
-      long gigasecond = 1000000000;
-      var sum = moment.AddSeconds(gigasecond);
-      return sum;
-
+        long gigasecond = 1000000000;
+        var sum = moment.AddSeconds(gigasecond);
+        return sum;
     }
 }
