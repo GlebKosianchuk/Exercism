c00303a945a2eecead517efaebcd92dc50f9395f
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-15 09:29:31 +0300

    gigasecond correct edition

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -3,8 +3,8 @@ public static class Gigasecond
 {
     public static DateTime Add(DateTime moment)
     {
-        long gigasecond = 1000000000;
-        var sum = moment.AddSeconds(gigasecond);
-        return sum;
+        var gigasecond = 1000000000;
+        var timeAfteraGigasecond = moment.AddSeconds(gigasecond);
+        return timeAfteraGigasecond;
     }
 }
