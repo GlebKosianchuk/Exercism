c801280ab6ef26405c9740c49956855e3afae4a6
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-16 12:37:19 +0300

    StringBuilder method

--- csharp/reverse-string/ReverseString.cs
+++ csharp/reverse-string/ReverseString.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿using System.Text;
 
 public static class ReverseString
 {
     public static string Reverse(string input)
     {
-        string letters = "";
-        for(var i = input.Length - 1; i >= 0; i --)
+        StringBuilder letters = new StringBuilder ("");
+        for(var i = input.Length - 1; i >= 0; i--)
         {
-            letters += input[i];
+            letters.Append(input[i]);
         }
-        return letters;
+        return letters.ToString();
     }
 }
