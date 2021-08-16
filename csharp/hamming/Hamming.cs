8d746620e4671c0ff3a83a3527bbf76ad5d88e4e
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-16 19:21:35 +0300

    Hamming done

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -4,6 +4,28 @@ public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-        throw new NotImplementedException("You need to implement this function.");
+    var collector = 0;
+        {
+            if (firstStrand.Length > secondStrand.Length)
+            throw new ArgumentException();
+        }
+        {
+            if (firstStrand.Length < secondStrand.Length)
+            throw new ArgumentException();
+        }
+        {
+            if ( firstStrand == "" && firstStrand.Length < secondStrand.Length)
+            throw new ArgumentException();
+        }
+        {
+             if ( secondStrand == "" && firstStrand.Length > secondStrand.Length)
+            throw new ArgumentException();
+        }
+            for (var i=0;i<=firstStrand.Length-1;i++)
+            {
+                if (firstStrand[i] != secondStrand[i])
+                collector=collector + 1;
+            }
+            return collector;
     }
 }
