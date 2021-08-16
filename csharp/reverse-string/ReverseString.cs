4cbd7258df370ad55390987f78edd1204a080e92
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-16 12:50:16 +0300

    Correct StrigBuilder (Done!)

--- csharp/reverse-string/ReverseString.cs
+++ csharp/reverse-string/ReverseString.cs
@@ -4,7 +4,7 @@ public static class ReverseString
 {
     public static string Reverse(string input)
     {
-        StringBuilder letters = new StringBuilder ("");
+        var letters = new StringBuilder();
         for(var i = input.Length - 1; i >= 0; i--)
         {
             letters.Append(input[i]);
