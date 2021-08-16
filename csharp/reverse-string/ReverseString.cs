1d01a4c0c901286a735581afc39db14a835af167
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-16 11:54:23 +0300

    Correct space

--- csharp/reverse-string/ReverseString.cs
+++ csharp/reverse-string/ReverseString.cs
@@ -5,10 +5,10 @@ public static class ReverseString
     public static string Reverse(string input)
     {
         string letters = "";
-            for(var i = input.Length - 1; i >= 0; i --)
+        for(var i = input.Length - 1; i >= 0; i --)
         {
             letters += input[i];
         }
-    return letters;
+        return letters;
     }
 }
