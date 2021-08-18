ef77a38f529722faf7b2a01e66fc5426b142d16f
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-18 10:30:17 +0300

    Series Complited Task

--- csharp/series/Series.cs
+++ csharp/series/Series.cs
@@ -1,9 +1,22 @@
+using System.Collections.Generic;
 using System;
-
 public static class Series
 {
     public static string[] Slices(string numbers, int sliceLength)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var someList = new List<string>();
+        var stroka = "";
+            if(numbers.Length < sliceLength || sliceLength <= 0)
+            throw new ArgumentException();
+        for(var i = 0; i <= numbers.Length - sliceLength; i++)
+        {
+            for( var k = i; k < i + sliceLength; k++)
+            {
+                stroka += numbers[k];
+            }
+            someList.Add(stroka);
+            stroka = "";
+        }
+        return someList.ToArray();
     }
 }
