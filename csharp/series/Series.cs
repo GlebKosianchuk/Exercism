65517667aa9bc5b1ac8d19fe5f1e9eebaebef37b
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-18 12:00:29 +0300

    Fixed all review comments

--- csharp/series/Series.cs
+++ csharp/series/Series.cs
@@ -1,21 +1,22 @@
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
+
 public static class Series
 {
     public static string[] Slices(string numbers, int sliceLength)
     {
-        var someList = new List<string>();
-        var stroka = "";
-            if(numbers.Length < sliceLength || sliceLength <= 0)
+        if(numbers.Length < sliceLength || sliceLength <= 0)
             throw new ArgumentException();
+
+        var someList = new List<string>();
         for(var i = 0; i <= numbers.Length - sliceLength; i++)
         {
-            for( var k = i; k < i + sliceLength; k++)
+            var series = "";
+            for(var k = i; k < i + sliceLength; k++)
             {
-                stroka += numbers[k];
+                series += numbers[k];
             }
-            someList.Add(stroka);
-            stroka = "";
+            someList.Add(series);
         }
         return someList.ToArray();
     }
