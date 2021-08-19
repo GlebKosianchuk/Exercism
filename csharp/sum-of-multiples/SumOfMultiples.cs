0f03072ec93cd98d8dff4c943c603583266f299a
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-19 17:55:40 +0300

    'do while' Operation

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-
+﻿using System.Collections.Generic;
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var listForSumOperation = new List<int>();
+        var sum = 0;
+        foreach (int operand in multiples)
+        {
+            var result = 0;
+            do
+            {
+                if (!listForSumOperation.Contains(result))
+                {
+                    listForSumOperation.Add(result);
+                }
+
+                result += operand;
+            } while (result < max && result != 0);
+        }
+
+        foreach (int multipless in listForSumOperation)
+        {
+            sum += multipless;
+        }
+        return sum;
     }
 }
