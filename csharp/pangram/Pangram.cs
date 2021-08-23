d1499a82dd50ebd9c995c1f6d1d7c54542ecfaa2
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-23 13:50:42 +0300

    Complited 'Pangram' Task

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+
+        char[] alph = new char[26];
+        for (int i = 'a'; i <= 'z'; i++)
+        {
+            alph[i - 'a'] = (char) i;
+        }
+
+        var copy = input.ToLower();
+        foreach (char letter in alph)
+            if (!copy.Contains(letter))
+            {
+                return false;
+            }
+        return true;
     }
 }
