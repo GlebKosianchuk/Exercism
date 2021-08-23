4dd2a73c827c9676475a729fa37176a0a3df5f88
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-23 14:00:50 +0300

    Fixed 'Pangram'

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -1,24 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-public static class Pangram
+﻿public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-
-        char[] alph = new char[26];
-        for (int i = 'a'; i <= 'z'; i++)
+        var alphabet = new char[26];
+        for (var i = 'a'; i <= 'z'; i++)
         {
-            alph[i - 'a'] = (char) i;
+            alphabet[i - 'a'] = (char) i;
         }
 
-        var copy = input.ToLower();
-        foreach (char letter in alph)
-            if (!copy.Contains(letter))
+        var inputInLower = input.ToLower();
+        foreach (var letter in alphabet)
+        {
+            if (!inputInLower.Contains(letter))
             {
                 return false;
+
             }
+        }
         return true;
     }
 }
