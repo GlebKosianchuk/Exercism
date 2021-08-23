0d8e0715276aa46b29eb09be00c0d01420bfa04a
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-23 14:11:23 +0300

    fixed 'Pangram' (Correct edition)

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -2,19 +2,13 @@
 {
     public static bool IsPangram(string input)
     {
-        var alphabet = new char[26];
-        for (var i = 'a'; i <= 'z'; i++)
-        {
-            alphabet[i - 'a'] = (char) i;
-        }
-
+        const string alphabet = "abcdefghijklmnopqrstuvwxyz";
         var inputInLower = input.ToLower();
         foreach (var letter in alphabet)
         {
             if (!inputInLower.Contains(letter))
             {
                 return false;
-
             }
         }
         return true;
