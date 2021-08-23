a88d7b3db0e555f371d0e8353bc5842be2d43586
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-23 19:11:08 +0300

    Complited 'QueenAttack'

---
+++ csharp/queen-attack/QueenAttack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Queen
+{
+    public Queen(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public int Row { get; }
+    public int Column { get; }
+}
+public static class QueenAttack
+{
+    public static bool CanAttack(Queen white, Queen black)
+    {
+        if ((white.Column == black.Column) || (white.Row == black.Row) || Math.Abs(white.Column - black.Column) == Math.Abs(white.Row - black.Row) ) 
+            return true;
+        else
+            return false;
+    }
+    public static Queen Create(int row, int column)
+    {
+        if (row >= 8 || row < 0 || (column >= 8 || column < 0))
+            throw new ArgumentOutOfRangeException();
+        var q = new Queen(row,column);
+        return q;
+    }
+}
