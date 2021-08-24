7d2257578b6ea1437d5067d26919f296cefc4221
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-24 12:47:13 +0300

    Complited 'Bob' Task

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -4,6 +4,29 @@ public static class Bob
 {
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var statemenntWithTrim = statement.Trim();
+        var copytToUpper = statement.ToUpper();
+
+        if (copytToUpper == statement && statement.EndsWith("?") && IsStringContainsLetter(statement))
+            return "Calm down, I know what I'm doing!";
+
+        if (statemenntWithTrim.EndsWith("?"))
+            return "Sure.";
+
+        if (statemenntWithTrim == "")
+            return "Fine. Be that way!";
+
+        if (statement == copytToUpper && IsStringContainsLetter(statement))
+            return "Whoa, chill out!";
+
+        return "Whatever.";
     }
+       public static  bool IsStringContainsLetter(string statement)
+        {
+            foreach (var letter in statement)
+                if (char.IsLetter(letter))
+                    return true;
+            return false;
+        }
+    
 }
