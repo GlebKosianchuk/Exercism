e006bc273f3be51ad9f890d3ccaa40ef989ac585
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-25 17:36:36 +0300

    fixed comments

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -5,19 +5,18 @@ public static class Bob
 {
     public static string Response(string statement)
     {
-        var statemenntWithTrim = statement.Trim();
         var isUpperCase = statement.ToUpper();
-        var isThereAnyLetters = statement.Any(s => char.IsLetter(s));
-        if (isUpperCase == statement && statement.EndsWith("?") && isThereAnyLetters)
+        var hasLetter = statement.Any(s => char.IsLetter(s));
+        if (isUpperCase == statement && statement.EndsWith("?") && hasLetter)
             return "Calm down, I know what I'm doing!";
 
-        if (statemenntWithTrim.EndsWith("?"))
+        if (statement.Trim().EndsWith("?"))
             return "Sure.";
 
         if (string.IsNullOrWhiteSpace(statement))
             return "Fine. Be that way!";
 
-        if (statement == isUpperCase && isThereAnyLetters)
+        if (statement == isUpperCase && hasLetter)
             return "Whoa, chill out!";
 
         return "Whatever.";
