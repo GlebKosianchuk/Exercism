3591af9fc3234911a4265443522bb2af70833f6f
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-25 16:03:09 +0300

    Complited 'Armstrong' task

--- csharp/armstrong-numbers/ArmstrongNumbers.cs
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -1,9 +1,26 @@
 ﻿using System;
-
+using System.Collections.Generic;
 public static class ArmstrongNumbers
 {
     public static bool IsArmstrongNumber(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var numberr = number;
+        var ListOfNumbers = new List<int>();
+        var i = 0;
+        double numbersInDegree = 0;
+
+        while (numberr % 10 > 0)
+        {
+            numberr /= 10;
+            i += 1;
+        }
+
+        foreach (var numbers in number.ToString())
+            ListOfNumbers.Add(int.Parse(numbers.ToString()));
+
+        foreach (var numbers in ListOfNumbers)
+            numbersInDegree += Math.Pow(numbers, i);
+
+       return  numbersInDegree == number || numbersInDegree == 1;
     }
 }
