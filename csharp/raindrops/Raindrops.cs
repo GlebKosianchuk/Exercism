9c8b9f924404d93fff13881aec5b8075e9e8fed5
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-27 10:49:05 +0300

    Complited task №1 , I am going to convert everything into 'Switch''

--- csharp/raindrops/Raindrops.cs
+++ csharp/raindrops/Raindrops.cs
@@ -4,6 +4,21 @@ public static class Raindrops
 {
     public static string Convert(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (number % 3 == 0 && number % 5 == 0 && number % 7 == 0)
+            return "PlingPlangPlong";
+        if (number % 3 == 0 && number % 5 == 0)
+            return "PlingPlang";
+        if (number % 3 == 0 && number % 7 == 0)
+            return "PlingPlong";
+        if (number % 5 == 0 && number % 7 == 0)
+            return "PlangPlong";
+        if (number % 3 == 0)
+            return "Pling";
+        if (number % 5 == 0)
+            return "Plang";
+        if (number % 7 == 0)
+            return "Plong";
+
+        return number.ToString();
     }
 }
