4955f1954680e5fd8e2741bd27164c845f0b8cb9
GlebKosianchuk <glebkosianchuk@mail.ru>
2021-08-28 10:41:57 +0300

    Done

--- csharp/collatz-conjecture/CollatzConjecture.cs
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public static class CollatzConjecture
 {
     public static int Steps(int number)
     {
-        throw new NotImplementedException("You need to implement this function");
+        var i = 0;
+        while (number != 1)
+        {
+            if (number == 0 || number < 0)
+                throw new ArgumentOutOfRangeException();
+            i += 1;
+            number = number % 2 == 0 ? number / 2 : 3 * number + 1;
+            continue;
+        }
+        return i;
     }
+    
 }
